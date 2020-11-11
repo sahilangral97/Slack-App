3622bc7cff7ae5d4b3bf2e28a54539d06f46acc7
sahilangral97 <sahil16082@iiitd.ac.in>
2020-11-12 01:17:45 +0530

    App-created

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                {/* user avatar*/}
+                {/* time logo*/}                
+            </div>
+            <div className="header__search">
+                {/* search logo*/}
+                {/* input field*/}
+            </div>
+            <div className="header__right">
+                {/* help logo*/}
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Header
